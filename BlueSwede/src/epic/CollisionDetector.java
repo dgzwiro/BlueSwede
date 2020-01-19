11c599e6a29b4fde85b81bfa3f0fe3f3fca4510d
kubase <emilia.kubas@ig.com>
2020-01-19 22:01:36 +0100

    CollisionDetector commit

--- BlueSwede/src/epic/CollisionDetector.java
+++ BlueSwede/src/epic/CollisionDetector.java
@@ -4,55 +4,35 @@ import epic.terrain.Terrain;
 
 public final class CollisionDetector {
 
-	public static boolean detectAll() {
+    public static boolean detectAll() {
 
-		return false;
+        return false;
+    }
+
+    public static boolean detectAgainst(Placeable arg0, Placeable arg1) {
+        if (arg0 == arg1) {
+            return false;
+        }
+
+		return ((arg0.getCoords()[0] >= arg1.getCoords()[0] && arg0.getCoords()[0] <= arg1.getCoords()[0] + arg1.getSize()) && (arg0.getCoords()[1] >= arg1.getCoords()[1] && arg0.getCoords()[1] <= arg1.getCoords()[1] + arg1.getSize()))
+				|| ((arg0.getCoords()[0] + arg0.getSize() >= arg1.getCoords()[0] && arg0.getCoords()[0] <= arg1.getCoords()[0] + arg1.getSize()) && (arg0.getCoords()[1] + arg0.getSize() >= arg1.getCoords()[1] && arg0.getCoords()[1] <= arg1.getCoords()[1] + arg1.getSize()))
+				|| (arg1.getCoords()[0] + arg1.getSize() >= arg0.getCoords()[0] && arg1.getCoords()[0] <= arg0.getCoords()[0] + arg0.getSize()) && (arg1.getCoords()[1] + arg1.getSize() >= arg0.getCoords()[1] && arg1.getCoords()[1] <= arg0.getCoords()[1] + arg0.getSize());
 	}
 
-	public static boolean detectAgainst(Placeable arg0, Placeable arg1) {
-		if(arg0 == arg1) {
-			return false;
-		}
-		
-		if (arg0.getCoords()[0] >= arg1.getCoords()[0] && arg0.getCoords()[0] <= arg1.getCoords()[0] + arg1.getSize()) {
-			if (arg0.getCoords()[1] >= arg1.getCoords()[1] && arg0.getCoords()[1] <= arg1.getCoords()[1] + arg1.getSize()) {
-				return true;
-			}
-		}
-		
-		if (arg0.getCoords()[0] + arg0.getSize() >= arg1.getCoords()[0] && arg0.getCoords()[0] <= arg1.getCoords()[0] + arg1.getSize()) {
-			if (arg0.getCoords()[1] + arg0.getSize() >= arg1.getCoords()[1] && arg0.getCoords()[1] <= arg1.getCoords()[1] + arg1.getSize()) {
-				return true;
-			}
-		}
-		
-		if (arg1.getCoords()[0] >= arg0.getCoords()[0] && arg1.getCoords()[0] <= arg0.getCoords()[0] + arg0.getSize()) {
-			if (arg1.getCoords()[1] >= arg0.getCoords()[1] && arg1.getCoords()[1] <= arg0.getCoords()[1] + arg0.getSize()) {
-				return true;
-			}
-		}
-		
-		if (arg1.getCoords()[0] + arg1.getSize() >= arg0.getCoords()[0] && arg1.getCoords()[0] <= arg0.getCoords()[0] + arg0.getSize()) {
-			if (arg1.getCoords()[1] + arg1.getSize() >= arg0.getCoords()[1] && arg1.getCoords()[1] <= arg0.getCoords()[1] + arg0.getSize()) {
-				return true;
-			}
-		}
-		
-		return false;
-	}
+    public static boolean detectAgainstAll(Placeable newPlaced) {
+//		System.out.println("Detecting for" +
 
-	public static boolean detectAgainstAll(Placeable newPlaced) {
-//		System.out.println("Detecting for" + newPlaced);
-		if(Main.getTerrain()[(int) (newPlaced.getCoords()[0]/50)][(int) (newPlaced.getCoords()[1]/50)].getType() == Terrain.Type.WATER){
-			return true;
-		}
-		
-		for(Placeable oldPlaced : Main.getAllPlacedObjects()){
-			if(detectAgainst(newPlaced, oldPlaced)){
-				return true;
-			}
-		}
-		return false;
-	}
+
+        if (Main.getTerrain()[(int) (newPlaced.getCoords()[0] / 50)][(int) (newPlaced.getCoords()[1] / 50)].getType() == Terrain.Type.WATER) {
+            return true;
+        }
+
+        for (Placeable oldPlaced : Main.getAllPlacedObjects()) {
+            if (detectAgainst(newPlaced, oldPlaced)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
 }
