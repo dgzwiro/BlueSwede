4dbde86ff9956abd6f473fd3e3eb850e30719e10
kubase <emilia.kubas@ig.com>
2020-01-19 20:51:34 +0100

    Tree class commit

--- BlueSwede/src/epic/enivormental/Tree.java
+++ BlueSwede/src/epic/enivormental/Tree.java
@@ -4,6 +4,7 @@ import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
 
 import javax.imageio.ImageIO;
@@ -13,109 +14,102 @@ import epic.Resource;
 
 public class Tree implements Resource {
 
-	private double coordX;
-	private double coordY;
-	private int value;
-	private static BufferedImage avatar;
+    private double coordX;
+    private double coordY;
+    private int value;
+    private static BufferedImage avatar;
+	private static final int SIZE = 10;
 
-	static {
+    static {
 
-		File srcImage = new File("BlueSwede/resource/img/tree.jpg");
-		try {
-			setAvatar(ImageIO.read(srcImage));
-		} catch (IOException e) {
-			System.err.println("Error in loading image");
-			e.printStackTrace();
-			setAvatar(new BufferedImage(20, 20, BufferedImage.TYPE_INT_RGB));
-		}
-	}
+        File srcImage = new File("BlueSwede/resource/img/tree.jpg");
+        try {
+            setAvatar(ImageIO.read(srcImage));
+        } catch (IOException e) {
+            System.err.println("Error in loading image");
+            e.printStackTrace();
+            setAvatar(new BufferedImage(20, 20, BufferedImage.TYPE_INT_RGB));
+        }
+    }
 
-	private static final int size = 10;
 
-	public Tree(int coordX, int coordY) {
-		setCoords(coordX, coordY);
-		setValue(2000);
-	}
+    public Tree(int coordX, int coordY) {
+        setCoords(coordX, coordY);
+        setValue(2000);
+    }
 
-	public Tree(int coordX, int coordY, int newValue) {
-		setCoords(coordX, coordY);
-		setValue(newValue);
-	}
+    public Tree(int coordX, int coordY, int newValue) {
+        setCoords(coordX, coordY);
+        setValue(newValue);
+    }
 
-	@Override
-	public double[] getCoords() {
-		double[] coords = { getCoordX(), getCoordY() };
-		return coords;
-	}
+    @Override
+    public double[] getCoords() {
+        return new double[]{getCoordX(), getCoordY()};
 
-	public double getCoordX() {
-		return coordX;
-	}
+    }
 
-	private void setCoordX(int coordX) {
-		this.coordX = coordX;
-	}
+    public double getCoordX() {
+        return coordX;
+    }
 
-	public double getCoordY() {
-		return coordY;
-	}
+    private void setCoordX(int coordX) {
+        this.coordX = coordX;
+    }
 
-	private void setCoordY(int coordY) {
-		this.coordY = coordY;
-	}
+    public double getCoordY() {
+        return coordY;
+    }
 
-	@Override
-	public boolean contains(Point2D point) {
-		if (point.getX() >= coordX && point.getX() <= coordX + size) {
-			if (point.getY() >= coordY && point.getY() <= coordY + size) {
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	public int getSize() {
-		return size;
-	}
+    private void setCoordY(int coordY) {
+        this.coordY = coordY;
+    }
 
-	public BufferedImage getAvatar() {
-		return avatar;
-	}
+    @Override
+    public boolean contains(Point2D point) {
+        return (point.getX() >= coordX && point.getX() <= coordX + SIZE) && (point.getY() >= coordY && point.getY() <= coordY + SIZE);
+    }
 
-	private static void setAvatar(BufferedImage avatar) {
-		Tree.avatar = avatar;
-	}
+    public int getSize() {
+        return SIZE;
+    }
 
-	@Override
-	public void setCoords(int coordX, int coordY) {
-		setCoordY(coordY);
-		setCoordX(coordX);
-	}
-	
-	private void setValue(int newValue) {
-		this.value = newValue;
-	}
+    public BufferedImage getAvatar() {
+        return avatar;
+    }
 
-	@Override
-	public int getValue() {
-		return value;
-	}
+    private static void setAvatar(BufferedImage avatar) {
+        Tree.avatar = avatar;
+    }
 
-	@Override
-	public String toString(){
-		return "X:" + coordX + "Y:" + coordY + "type:TREEValue:" + getValue();
-	}
+    @Override
+    public void setCoords(int coordX, int coordY) {
+        setCoordY(coordY);
+        setCoordX(coordX);
+    }
 
-	@Override
-	public BufferedImage getAvatarBig() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    private void setValue(int newValue) {
+        this.value = newValue;
+    }
 
-	@Override
-	public List<Orders> getOrders() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return "X:" + coordX + "Y:" + coordY + "type:TREEValue:" + getValue();
+    }
+
+    @Override
+    public BufferedImage getAvatarBig() {
+        return null;
+    }
+
+    @Override
+    public List<Orders> getOrders() {
+        return Collections.emptyList();
+    }
 
 }
