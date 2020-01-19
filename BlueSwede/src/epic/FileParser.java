aa0da4584a5a2f2411d99e0e4bc53411fe898be9
kubase <emilia.kubas@ig.com>
2020-01-19 21:56:49 +0100

    FileParser commit

--- BlueSwede/src/epic/FileParser.java
+++ BlueSwede/src/epic/FileParser.java
@@ -2,7 +2,6 @@ package epic;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -14,132 +13,87 @@ import epic.terrain.Terrain;
 
 public class FileParser {
 
-	private static Pattern mapPattern = Pattern
-			.compile("MapsizeX:(\\d+).*MapsizeY:(\\d+).*PlacedObjects(.*)TerrainObjects(.*)");
-	private static Pattern placeablePattern = Pattern.compile("X:(\\d+).*Y:(\\d+).*type:([A-Z]+)Value:(\\d+)");
-	private static Pattern terrainPattern = Pattern.compile("X(\\d+)Y(\\d+)([A-Z]+)");
-	private static String fileLocation = "map.txt";
-	
-	public static void saveMapFile() {
-		//System.out.println("Trying to save file");
+    private static Pattern mapPattern = Pattern
+            .compile("MapsizeX:(\\d+).*MapsizeY:(\\d+).*PlacedObjects(.*)TerrainObjects(.*)");
+    private static Pattern placeablePattern = Pattern.compile("X:(\\d+).*Y:(\\d+).*type:([A-Z]+)Value:(\\d+)");
+    private static Pattern terrainPattern = Pattern.compile("X(\\d+)Y(\\d+)([A-Z]+)");
 
-		StringBuilder fileContent = new StringBuilder();
+    public static void loadMapFile() {
+        String fileLocation = "map.txt";
+        File file = new File(fileLocation);
+        System.out.println(file.getAbsolutePath());
+        Scanner in = null;
+        StringBuilder bufferedInput = new StringBuilder();
 
-		fileContent.append("MapsizeX:" + Main.getMapTilesX() + "MapsizeY:" + Main.getMapTilesY());
-		fileContent.append("PlacedObjects");
-		for (Placeable object : Main.getAllPlacedObjects()) {
-			fileContent.append(object);
-			fileContent.append("#");
-		}
+        try {
+            in = new Scanner(file);
+            while (in.hasNextLine()) {
+                bufferedInput.append(in.nextLine());
+                bufferedInput.append("\n");
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+        }
 
-		fileContent.append("TerrainObjects");
-		for (int i = 0; i < Main.getMapTilesX(); i++) {
-			for (int j = 0; j < Main.getMapTilesY(); j++) {
-				fileContent.append("X" + i + "Y" + j + Main.getTerrain()[i][j]);
-				fileContent.append("#");
-			}
-		}
+        String input = bufferedInput.toString();
 
-		PrintWriter savingFile = null;
-		try {
-			savingFile = new PrintWriter(fileLocation);
-			savingFile.println(fileContent);
-			//System.out.println("File saved");
-		} catch (FileNotFoundException e) {
-			//System.out.println("File not found.");
-			e.printStackTrace();
-		} finally {
-			savingFile.close();
-		}
-	}
+        Matcher mapMatch = mapPattern.matcher(input);
+        if (mapMatch.lookingAt()) {
+            int mapSizeX = Integer.parseInt(mapMatch.group(1));
+            int mapSizeY = Integer.parseInt(mapMatch.group(2));
+            String placeables = mapMatch.group(3);
+            String tiles = mapMatch.group(4);
 
-	public static void loadMapFile() {
-		File file = new File(fileLocation);
-		System.out.println(file.getAbsolutePath());
-		Scanner in = null;
-		StringBuilder bufferedInput = new StringBuilder();
+            List<Placeable> loadedPlaceables = new ArrayList<>();
+            for (String placeable : placeables.split("#")) {
+                Matcher placeableMatch = placeablePattern.matcher(placeable);
+                if ((placeableMatch.lookingAt()) && ("TREE".equals(placeableMatch.group(3)))) {
+                    Tree loadedTree = new Tree(Integer.parseInt(placeableMatch.group(1)),
+                            Integer.parseInt(placeableMatch.group(2)), Integer.parseInt(placeableMatch.group(4)));
+                    loadedPlaceables.add(loadedTree);
+                }
 
-		try {
-			in = new Scanner(file);
-			while (in.hasNextLine()) {
-				bufferedInput.append(in.nextLine());
-				bufferedInput.append("\n");
-			}
-			//System.out.println("File loaded");
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} finally {
-			in.close();
-		}
+            }
 
-		String input = bufferedInput.toString();
+            Terrain[][] loadedTerrains = new Terrain[mapSizeX][mapSizeY];
+            for (String tile : tiles.split("#")) {
+                Matcher terrainMatch = terrainPattern.matcher(tile);
 
-		Matcher mapMatch = mapPattern.matcher(input);
-		if (mapMatch.lookingAt()) {
-			int mapSizeX = Integer.parseInt(mapMatch.group(1));
-			int mapSizeY = Integer.parseInt(mapMatch.group(2));
-			String placeables = mapMatch.group(3);
-			String tiles = mapMatch.group(4);
+                if (terrainMatch.lookingAt()) {
+                    Terrain loadedTile = new Terrain();
+                    switch (terrainMatch.group(3)) {
+                        case "WATER":
+                            loadedTile.setTile(Terrain.Type.WATER);
+                            loadedTerrains[Integer.parseInt(terrainMatch.group(1))][Integer.parseInt(terrainMatch.group(2))] = loadedTile;
+                            break;
+                        case "GRASS":
+                            loadedTile.setTile(Terrain.Type.GRASS);
+                            loadedTerrains[Integer.parseInt(terrainMatch.group(1))][Integer.parseInt(terrainMatch.group(2))] = loadedTile;
+                            break;
+                        case "SAND":
+                            loadedTile.setTile(Terrain.Type.SAND);
+                            loadedTerrains[Integer.parseInt(terrainMatch.group(1))][Integer.parseInt(terrainMatch.group(2))] = loadedTile;
+                            break;
+                        case "MOUNTAIN":
+                            loadedTile.setTile(Terrain.Type.MOUNTAIN);
+                            loadedTerrains[Integer.parseInt(terrainMatch.group(1))][Integer.parseInt(terrainMatch.group(2))] = loadedTile;
+                            break;
+                        default:
+                            break;
 
-			List<Placeable> loadedPlaceables = new ArrayList<Placeable>();
-			for (String placeable : placeables.split("#")) {
-				//System.out.println(placeable);
-				Matcher placeableMatch = placeablePattern.matcher(placeable);
-				if (placeableMatch.lookingAt()) {
+                    }
+                }
+            }
 
-					switch (placeableMatch.group(3)) {
-					case "TREE":
-						Tree loadedTree = new Tree(Integer.parseInt(placeableMatch.group(1)),
-								Integer.parseInt(placeableMatch.group(2)), Integer.parseInt(placeableMatch.group(4)));
-						loadedPlaceables.add(loadedTree);
-						break;
-					default:
-						break;
-					}
-				} else {
-					//System.out.println("LINE DOES NOT MATCH!");
-				}
+            Main.loadMap(mapSizeX, mapSizeY, loadedTerrains, loadedPlaceables);
 
-			}
-
-			Terrain[][] loadedTerrains = new Terrain[mapSizeX][mapSizeY];
-			for (String tile : tiles.split("#")) {
-				//System.out.println(tile);
-				Matcher terrainMatch = terrainPattern.matcher(tile);
-
-				if (terrainMatch.lookingAt()) {
-					Terrain loadedTile = new Terrain();
-					switch (terrainMatch.group(3)) {
-					case "WATER":
-						loadedTile.setTile(Terrain.Type.WATER);
-						loadedTerrains[Integer.parseInt(terrainMatch.group(1))][Integer.parseInt(terrainMatch.group(2))]=loadedTile;
-						break;
-					case "GRASS":
-						loadedTile.setTile(Terrain.Type.GRASS);
-						loadedTerrains[Integer.parseInt(terrainMatch.group(1))][Integer.parseInt(terrainMatch.group(2))]=loadedTile;
-						break;
-					case "SAND":
-						loadedTile.setTile(Terrain.Type.SAND);
-						loadedTerrains[Integer.parseInt(terrainMatch.group(1))][Integer.parseInt(terrainMatch.group(2))]=loadedTile;
-						break;
-					case "MOUNTAIN":
-						loadedTile.setTile(Terrain.Type.MOUNTAIN);
-						loadedTerrains[Integer.parseInt(terrainMatch.group(1))][Integer.parseInt(terrainMatch.group(2))]=loadedTile;
-						break;
-					default:
-						break;
-
-					}
-				} else {
-					//System.out.println("LINE DOES NOT MATCH!");
-				}
-			}
-			
-			Main.loadMap(mapSizeX, mapSizeY, loadedTerrains, loadedPlaceables);
-
-		} else {
-			System.err.println("FILE CORRUPTED");
-		}
-	}
+        } else {
+            System.err.println("FILE CORRUPTED");
+        }
+    }
 
 }
